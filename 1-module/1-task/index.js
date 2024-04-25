b95370b833c184c4de2d818f58f538e45afa388c
Ildar Gataullin <ildarado12@yandex.ru>
2024-04-26 00:00:22 +0300

    Функция для вычисления факториала.

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,16 @@
 function factorial(n) {
-  // ваш код...
+  if (n < 0) {
+    alert(`Введите положительное число или ноль.`);
+    return;
+  } if (n > 1) {
+    let i = n; 
+    while (i > 1) {
+      n *= (i - 1);
+      --i;
+    }
+  } else {
+    n = 1;
+  }
+  return n;
 }
+alert(factorial(1));
