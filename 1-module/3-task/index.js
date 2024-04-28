3db5d04cdc2232c604d3d093365bd21cc2a203dd
Ildar Gataullin <ildarado12@yandex.ru>
2024-04-28 14:06:17 +0300

    Выполнены задачи с 2 до 5

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,11 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str.length == 0) {
+    return upperStr = ``;
+  } else {
+    let upperStr = str[0].toUpperCase();
+    for (let i = 1; str.length > i; i++) {
+      upperStr = upperStr + str[i]; 
+    }
+    return upperStr;
+  }
 }
