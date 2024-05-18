95763d447df36417d1b3fb47f1fce8997d6c9d32
Ildar Gataullin <ildarado12@yandex.ru>
2024-05-18 16:36:18 +0300

    Выполнена 3 задача в 5м модуле

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,32 @@
 function initCarousel() {
-  // ваш код...
+  let carouselArrowRight = document.querySelector(`.carousel__arrow_right`);
+  let carouselArrowLeft = document.querySelector(`.carousel__arrow_left`);
+  let carouselInner = document.querySelector(`.carousel__inner`);
+  let widthCarouselInner = carouselInner.offsetWidth;
+  let levelCarouselInner = 1;
+  carouselArrowLeft.style.display = 'none';
+
+  carouselArrowRight.addEventListener(`click`, () => {
+    levelCarouselInner += 1;
+    carouselInner.style.transform = `translateX(-${(levelCarouselInner - 1) * widthCarouselInner}px)`;
+    if (levelCarouselInner < 4) {
+      carouselArrowRight.style.display = '';
+      carouselArrowLeft.style.display = '';      
+    } else {
+      carouselArrowRight.style.display = 'none';
+    }
+    console.log(levelCarouselInner);
+  });
+
+  carouselArrowLeft.addEventListener(`click`, () => {
+    levelCarouselInner -= 1;
+    carouselInner.style.transform = `translateX(-${(levelCarouselInner - 1) * widthCarouselInner}px)`;
+    if (levelCarouselInner <= 1) {
+      carouselArrowLeft.style.display = 'none';
+    } else {
+      carouselArrowLeft.style.display = '';
+      carouselArrowRight.style.display = '';
+    }
+    console.log(levelCarouselInner);
+  });
 }
