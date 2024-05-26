60cd8258826aec6e674351085dbe09b0b07a96d0
Ildar Gataullin <ildarado12@yandex.ru>
2024-05-26 17:15:28 +0300

    Выполнена 2 задача в 6м модуле. Удален console.log из 1 задачи 6 модуля

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,40 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
   constructor(product) {
+    this.product = product;
+    this.render();
+    this.customEvent();
+  }
+
+  render() {
+    this.elem = createElement(`
+    <div class="card">
+      <div class="card__top">
+        <img src="/assets/images/products/${this.product.image}" class="card__image" alt="product">
+        <span class="card__price">€${this.product.price.toFixed(2)}</span>
+      </div>
+      <div class="card__body">
+        <div class="card__title">${this.product.name}</div>
+        <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+      </div>
+    </div>
+    `);
+    return this.elem;
+  }
+
+  customEvent() {
+    let classCardButton = this.elem.querySelector(`.card__button`);
+    classCardButton.addEventListener(`product-add`, ev => {});
+    classCardButton.addEventListener(`click`, ev => {
+      let MyEvent = new CustomEvent(`product-add`, {
+        detail: this.product.id,
+        bubbles: true
+      });
+      classCardButton.dispatchEvent(MyEvent);
+    }
+    );
   }
 }
