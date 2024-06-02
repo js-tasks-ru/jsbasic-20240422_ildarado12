d040acbadebe9b41529ea2cb816f172d7cdaf736
Ildar Gataullin <ildarado12@yandex.ru>
2024-06-02 13:38:20 +0300

    Решена 2 и 3 задача 7 модуля, но тест не принимает

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,77 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.elem = createElement(`
+    <div class="modal">
+      <!--Прозрачная подложка перекрывающая интерфейс-->
+      <div class="modal__overlay"></div>
+  
+      <div class="modal__inner">
+        <div class="modal__header">
+          <!--Кнопка закрытия модального окна-->
+          <button type="button" class="modal__close">
+            <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+          </button>
+  
+          <h3 class="modal__title">
+            Вот сюда нужно добавлять заголовок
+          </h3>
+        </div>
+  
+        <div class="modal__body">
+          A сюда нужно добавлять содержимое тела модального окна
+        </div>
+      </div>
+  
+    </div>
+    `);
+
+    let eventEsc = ev => {
+      if (ev.code === 'Escape') {
+        this.close();
+      }
+      ev.preventDefault();
+    };
+    this.eventEsc = eventEsc;
+    this.modalClose();
+    this.eventKeyEsc();
   }
+
+  open() {
+    let containerElem = document.querySelector(`.container`);
+    document.body.classList.add(`is-modal-open`);
+
+    return containerElem.append(this.elem);
+  }
+
+  setTitle(title) {
+    let modalTitleElem = this.elem.querySelector(`.modal__title`);
+    modalTitleElem.textContent = title;
+  }
+
+  setBody(domElem) {
+    let modalBodyElem = this.elem.querySelector(`.modal__body`);
+    modalBodyElem.textContent = ``;
+    modalBodyElem.append(domElem);
+  }
+
+  close() {
+    document.removeEventListener('keydown', this.eventEsc);
+
+    let modalElem = document.querySelector(`.modal`);
+    document.querySelector(`.container`).removeChild(modalElem);
+
+    document.body.classList.remove(`is-modal-open`);
+  } 
+
+  modalClose () {
+    let modalClose = this.elem.querySelector(`.modal__close`);
+    modalClose.addEventListener(`click`, ev => {
+      this.close();
+    });
+  }
+
+  eventKeyEsc() {
+    document.addEventListener('keydown', this.eventEsc);
+  } 
 }
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,84 @@
+import createElement from "../../assets/lib/create-element.js";
+
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.steps = steps;
+    this.value = value;
+    this.render();
+    this.changeSteps();
+    this.castomEvent();
+  }
+
+  render() {
+    this.elem = createElement(`
+    <div class="slider">
+
+      <!--Ползунок слайдера с активным значением-->
+      <div class="slider__thumb">
+        <span class="slider__value">${this.value}</span>
+      </div>
+
+      <!--Полоска слайдера-->
+      <div class="slider__progress"></div>
+
+      <!-- Шаги слайдера (вертикальные чёрточки) -->
+      <div class="slider__steps">
+      </div>
+    </div>`);
+
+    let sliderSteps = this.elem.querySelector(`.slider__steps`);
+    for (let i = 0; i < this.steps; i++) {
+      if (i == this.value) {
+        sliderSteps.append(createElement(`<span class="slider__step-active"></span>`));
+      } else {
+        sliderSteps.append(document.createElement(`SPAN`));
+      }
+    }
+
+    return this.elem;
+  }
+
+  changeSteps() {
+    let sliderElem = this.elem.querySelector(`.slider__thumb`).parentNode;
+    sliderElem.addEventListener(`click`, ev => {
+      let sliderPageX = sliderElem.getBoundingClientRect().x;
+      let sliderWidth = sliderElem.getBoundingClientRect().width;
+      let leftPercents = Math.round((ev.clientX - sliderPageX) / sliderWidth * 100);
+
+      this.sliderValue = Math.round(leftPercents / 100 * (this.steps - 1));
+      let spanValue = this.elem.querySelector(`.slider__value`);
+      spanValue.textContent = this.sliderValue;
+
+      let sliderSteps = this.elem.querySelector(`.slider__steps`);
+      let arraySpan = Array.from(sliderSteps.childNodes).filter(item => item.nodeName == `SPAN`);
+
+      for (let i = 0; i < arraySpan.length; i++) {
+        arraySpan[i].className = ``;
+        if (i == this.sliderValue) {
+          arraySpan[i].className = `slider__step-active`;          
+        }
+      }
+
+      let thumb = this.elem.querySelector('.slider__thumb');
+      let progress = this.elem.querySelector('.slider__progress');
+
+      thumb.style.left = `${leftPercents}%`;
+      progress.style.width = `${leftPercents}%`;
+    });
+  }
+
+  castomEvent() {
+    let sliderElem = this.elem.querySelector(`.slider__thumb`).parentNode;
+
+    sliderElem.addEventListener(`slider-change`, ev => {
+    });
+
+    sliderElem.addEventListener(`click`, ev => {
+      let MyEvent = new CustomEvent('slider-change', {
+        detail: this.sliderValue,
+        bubbles: true,
+      });
+      sliderElem.dispatchEvent(MyEvent);
+    });
   }
 }
