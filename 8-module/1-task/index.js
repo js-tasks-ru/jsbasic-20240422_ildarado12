8d5cecdafbf838840d9926453f2bb3dc50298d77
Ildar Gataullin <ildarado12@yandex.ru>
2024-06-26 19:22:59 +0300

    выполнены 2 задачи 8 модуля

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -49,8 +49,10 @@ export default class CartIcon {
       this.elem.style.position = `fixed`;
       this.elem.style.zIndex = 1000;
       if (marginContainerElem < 30 + this.elem.offsetWidth) {
-        this.elem.style.left = ``;
-        this.elem.style.right = `10px`;
+        this.elem.style.left = Math.min(
+          document.querySelector('.container').getBoundingClientRect().right + 20,
+          document.documentElement.clientWidth - this.elem.offsetWidth - 10
+        ) + 'px';
       } else {
         this.elem.style.left = marginContainerElem + containerElem.offsetWidth + 20 + `px`;
       }
